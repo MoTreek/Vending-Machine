305c0207780673a9e05af230ff0992c8d781994b
Matt Smith <matthew.smith60657@yahoo.com>
2022-06-09 12:55:25 -0400

    Inputs!

--- src/main/java/com/techelevator/FileScanner.java
+++ src/main/java/com/techelevator/FileScanner.java
@@ -1,34 +1,63 @@
 package com.techelevator;
 
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.List;
 import java.util.Scanner;
 
 public class FileScanner {
+    // 'out' will connect to the log file for... logging
     private PrintWriter out;
+    // 'in' will connect to the inventory file for parsing
     private Scanner in;
 
-    public FileScanner(InputStream input, OutputStream output) {
-        this.out = new PrintWriter(output);
-        this.in = new Scanner(input);
+    public FileScanner(String input, String output) {
+        // Need to take a string & create a FileOutputStream
+        this.out = getPrintWriter(output);
+        // Need to take a string & create a Scanner connected to a file named String
+        this.in = getScanner(input);
     }
 
-    public FileScanner() {
-
+    public Scanner getScanner(String input) {
+        try {
+            File inputFile = new File(input);
+            return new Scanner(inputFile);
+        } catch (FileNotFoundException fnfex) {
+            System.out.println(fnfex.getMessage());
+        }
     }
 
-    public List<Snack> inventoryFiles(){
-        try{
-            while(in.hasNextLine()){
+    private PrintWriter getPrintWriter (String outputFile) {
+        File newFile = new File(outputFile);
+
+        try {
+            if(!newFile.exists()) {
+                newFile.createNewFile();
+            }
+        } catch (IOException ioex) {
+            System.out.println(ioex.getMessage());
+        }
+        try (PrintWriter printWriter = new PrintWriter(new FileOutputStream(newFile, false))) {
+            return printWriter;
+        } catch (FileNotFoundException fnfex) {
+            System.out.println(fnfex.getMessage());
+        }
+        return null;
+    }
+
+    public List<Snack> inventoryFiles() {
+        // Create the empty List of Snacks
+
+        // This will take the 'in' Scanner, and parse to create & return a List of Snacks
+        try {
+            while (in.hasNextLine()) {
                 String[] array = in.nextLine().split("\\|");
                 System.out.println(array[0] + " " + array[1] + " " + array[2] + " " + array[3]);
             }
         } catch (Exception e) {
             System.out.println("Exception " + e.getMessage());
         }
+        // Return the complete List of Snacks
         return null;
     }
 
